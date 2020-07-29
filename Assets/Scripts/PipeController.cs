06fedbf9ba63469779e9781bf1715021afa73c5a
Karavopano <theosav86@hotmail.com>
2020-07-29 12:57:05 +0300

    Object Pooling Beta (working)

--- Assets/Scripts/PipeController.cs
+++ Assets/Scripts/PipeController.cs
@@ -7,6 +7,8 @@ public class PipeController : MonoBehaviour
 
     private Vector3 pipeOffSet = new Vector3(0, 0, 100f);
 
+    public Transform newPipeSpawnPoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,16 +21,11 @@ public class PipeController : MonoBehaviour
         
     }
 
-    private void OnCollisionExit(Collision collider)
+    private void OnTriggerExit(Collider collider)
     {
         if(collider.gameObject.CompareTag("Player"))
         {
             PipePool.Instance.ReturnToPool(this);
         }
-
-        PipeController newPipe = PipePool.Instance.Get();
-        newPipe.transform.position = transform.position + pipeOffSet;
-        newPipe.transform.rotation = transform.rotation;
-        newPipe.gameObject.SetActive(true);
     }
 }
---
+++ Assets/Scripts/PipeSpawnPointController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawnPointController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    { 
+
+        if (collider.CompareTag("Player"))
+        {
+            PipeController newPipe = PipePool.Instance.Get();
+            newPipe.transform.position = transform.position;
+            newPipe.transform.rotation = transform.rotation;
+            newPipe.gameObject.SetActive(true);
+
+            Debug.Log("Spawning new pipe");
+        }
+    }
+}
